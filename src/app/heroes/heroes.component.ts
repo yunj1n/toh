da329c43a2358e7139515b49eca44450e8bb8893
jinseong <alslghzm1@naver.com>
2018-10-20 15:19:27 +0900

    commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.sass']
 })
 export class HeroesComponent implements OnInit {
+  // 리터럴 객체로 객체 생성
+  /*hero: Hero = {
+    id: 11,
+    name: 'Winstorm'
+  };*/
+  hero: Hero;
 
-  constructor() { }
+  isSpecial = true;
+
+  constructor() {
+    // new 로 객체 생성
+    // this 는 객체 인스턴스 자기자신을 가르킨다
+    this.hero = new Hero();
+    this.hero.id = 11;
+    this.hero.name = 'Winstorm';
+  }
 
   ngOnInit() {
+  }
+
+  onSave(event) {
+    console.log(event);
   }
 
 }
